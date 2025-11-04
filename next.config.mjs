465989ceed8a88a819d9edb74bf5a66edb81450f
Viraj Mhadgut <mhadgutvirajwork@gmail.com>
2025-11-05 02:40:07 +0530

    Initial commit for new repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',        // enables static export
-  images: {
-    unoptimized: true,     // needed for GitHub Pages
-  },
-  trailingSlash: true,     // ensures all URLs end with /
-  distDir: 'out',          // ensures /out folder gets created
+  /* config options here */
 };
 
 export default nextConfig;
